b4b30867db2d59e04b2a734fc531e726e206c990
mayumiyamada <y.mayumi1011@gmail.com>
2021-02-01 23:40:02 +0900

    ユーザー管理機能 実装

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :email, uniqueness: true
+  validates :nickname, presence: true     
+  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}          
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/}    
+  validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}       
+  validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/}    
+  validates :birthday, presence: true
+
+  validates :encrypted_password,:password,:password_confirmation,format:{with:  /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
+  validates :password, confirmation: true
+
+end
