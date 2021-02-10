25cfb5d78955e6eac7dad95fc4ff544dde3a9c73
mayumiyamada <y.mayumi1011@gmail.com>
2021-02-10 20:33:43 +0900

    Rubocop　ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true     
-  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters'}          
-  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters'}    
-  validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}       
-  validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters'}    
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   validates :birthday, presence: true
- 
-  validates :password, presence: true, length:{minimum:6},
-             format:{with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/,message: 'Include both letters and numbers'}
+
+  validates :password, presence: true, length: { minimum: 6 },
+                       format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/, message: 'Include both letters and numbers' }
   validates :email, uniqueness: { case_sensitive: false }
-
-
 end
