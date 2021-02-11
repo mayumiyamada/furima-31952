b8627b3b7c62b8e9395537e75850b3973fe19da3
mayumiyamada <y.mayumi1011@gmail.com>
2021-02-11 22:01:44 +0900

    with_optionsの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,18 @@ class User < ApplicationRecord
 
   with_options presence: true do
   validates :nickname
-  validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' }
-  validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' }
   validates :birthday
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'Full-width characters' } do
+  validates :last_name
+  validates :first_name
   end
 
+  with_options  format: { with: /\A[ァ-ヶー－]+\z/, message: 'Full-width katakana characters' } do
+  validates :last_name_kana
+  validates :first_name_kana
+  end
+  
   validates :password, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}\z/, message: 'Include both letters and numbers' }
+end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     last_name             { '山田' }
     first_name            { 'まゆみ' }
     last_name_kana        { 'アアア' }
