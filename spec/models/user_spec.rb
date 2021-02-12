a28e7bee9b2e47b4737f4eedd0fed8e96f9197d3
mayumiyamada <y.mayumi1011@gmail.com>
2021-02-12 11:22:31 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,19 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
-    it 'nicknameが空では登録できない' do
+    context '新規登録できるとき' do
+      it 'nicknameとemail、passwordとpassword_confirmation、last_nameとfirst_name、last_name_kanaとfirst_name_kanaが存在すれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordとpassword_confirmationが半角英数字混合の6文字以上であれば登録できる' do
+        @user.password = '000aaa'
+        @user.password_confirmation = '000aaa'
+        expect(@user).to be_valid
+      end
+  end
+
+    context '新規登録できないとき' do
+     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Nickname can't be blank")
@@ -51,8 +63,28 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
+    it 'passwordとpassword_confirmationが一致しない場合、登録できない' do
+      @user.password = '000aaa'
+      @user.password_confirmation = '000bbb'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+
     it 'passwordは半角英数字混合でないと登録できない' do
       @user.password = 'aaaaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+    end
+
+    it 'passwordは数字のみ（６桁）は登録できない' do
+      @user.password = '111111'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
+    end
+
+    it 'passwordは全角英数字混合は登録できない' do
+      @user.password = '０００ＢＢＢ'
       @user.valid?
       expect(@user.errors.full_messages).to include('Password Include both letters and numbers')
     end
@@ -109,6 +141,7 @@ RSpec.describe User, type: :model do
       @user.birthday = ''
       @user.valid?
       expect(@user.errors.full_messages).to include("Birthday can't be blank")
+     end
     end
-  end
+ end
 end
