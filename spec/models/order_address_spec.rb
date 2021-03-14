13a63d14e1a1d80bec87ff8e934db8073a65ff5d
mayumiyamada <y.mayumi1011@gmail.com>
2021-03-14 16:00:23 +0900

    テストコード追加

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -85,6 +85,14 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include ("Phone number No hyphen(-)& less than 11")
       end
+
+      it "tokenが空では登録できないこと" do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+      
+
   end
  end
 end
