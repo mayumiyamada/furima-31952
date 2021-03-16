e2adc98c8a219775a437054e18ab23759101fd49
mayumiyamada <y.mayumi1011@gmail.com>
2021-03-16 23:43:58 +0900

    条件分岐訂正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,7 +2,7 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_order, only: [:index, :create]
   before_action :move_to_index, only: [:index, :create]
-  before_action :move_to_toppage, only: [:index, :create]
+
 
   def index
     @order_address = OrderAddress.new
@@ -36,9 +36,6 @@ class OrdersController < ApplicationController
     redirect_to root_path if current_user.id == @item.user_id
   end
 
-  def move_to_toppage
-    redirect_to root_path unless @item.order.nil?
-  end
 
   def set_order
     @item = Item.find(params[:item_id])
