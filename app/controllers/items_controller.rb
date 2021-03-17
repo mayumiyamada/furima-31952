0cf82e3a662734fbebcf2668c04951b4ffbed4eb
mayumiyamada <y.mayumi1011@gmail.com>
2021-03-17 15:36:34 +0900

    演算子　訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -52,7 +52,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless current_user.id == @item.user_id
+    redirect_to root_path if current_user.id == @item.user_id && @item.order.present?
   end
 
 
