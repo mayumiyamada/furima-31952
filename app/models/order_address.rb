bce234ba75c5505bfcf8843192a24316a3549b07
mayumiyamada <y.mayumi1011@gmail.com>
2021-03-18 13:09:01 +0900

    訂正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -18,6 +18,6 @@ class OrderAddress
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address,
-                   building_name: building_name, phone_number: phone_number, order: order.id)
+                   building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end
